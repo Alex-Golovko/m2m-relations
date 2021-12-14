0b415c985097d61fd2d6caf324e2c4149fc508d2
alex <ya.golovko-alex2017@yandex.ru>
2021-12-14 16:00:23 +0300

    init commit

---
+++ website/setting_local.py
@@ -0,0 +1,15 @@
+
+
+SECRET_KEY = 'b0e@^m&tccz11$w59qov$lhn-97!(%wfn-gray-c*x)^a$wx=2'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'netology_dj_hw',
+        'USER': 'alex',
+        'PASSWORD': '12345678',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
